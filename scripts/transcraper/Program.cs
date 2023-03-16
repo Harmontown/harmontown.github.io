613b63f34aecf833cfcc25e13b07faa4aa0b115c
stubn0rtz <126830942+stubn0rtz@users.noreply.github.com>
2023-03-16 14:35:56 +0000

    unboxed all the vtt files.

--- scripts/transcraper/Program.cs
+++ scripts/transcraper/Program.cs
@@ -1,27 +1,23 @@
 ﻿using System.Text.Json;
-using System.Text.Json.Serialization;
-using System.Diagnostics;
-using System.Linq;
-using System.Xml.Linq;
-using System.Text.RegularExpressions;
-using System.Net.Http.Headers;
 
 const string UrlFormat = "https://harmondev.duckdns.org/api/vtt/stream/Harmontown.S01E{0:D3}.vtt";
 const string FileFormat = "../../data/raw/harmondev.duckdns.org/Harmontown.S01E{0:D3}.vtt";
 
-var client = new HttpClient();
-for (int i = 1; i <= 361; i++)
-{
-  var url = string.Format(UrlFormat, i);
-  var filename = string.Format(FileFormat, i);
+// await Scrape();
 
-  var targetFile = new FileInfo(filename);
-  using (var stream = await client.GetStreamAsync(url))
+async Task Scrape()
+{
+  var client = new HttpClient();
+  for (int i = 1; i <= 361; i++)
   {
-    using (var fileStream = targetFile.Open(FileMode.Create, FileAccess.Write, FileShare.Read))
+    var url = string.Format(UrlFormat, i);
+    var filename = string.Format(FileFormat, i);
+
+    var targetFile = new FileInfo(filename);
+    using (var stream = await client.GetStreamAsync(url))
     {
-      await stream.CopyToAsync(fileStream);
-      await Task.Delay(TimeSpan.FromSeconds(1));
+      var response = await JsonDocument.ParseAsync(stream);
+      File.WriteAllText(targetFile.FullName, response.RootElement.GetProperty("content").GetString());
     }
   }
 }
