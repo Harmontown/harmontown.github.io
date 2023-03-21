54d0563d69c095c0ea68faeb7264ac1c1391c5e5
Stubn0rtz <126830942+stubn0rtz@users.noreply.github.com>
2023-03-21 20:54:57 +0000

    Fixed prev/next navigation

--- docs/_plugins/episodeAnnotationsGen.rb
+++ docs/_plugins/episodeAnnotationsGen.rb
@@ -2,17 +2,24 @@ module Harmontown
   class EpisodeAnnotationsGen < Jekyll::Generator
     def generate(site)
 
+      episodes = site.collections['episodes'].docs.sort_by { |ep| ep.data['sequenceNumber'] }
+      
       prefixLength = '/srv/jekyll'.length
-
-      episodes = site.collections['episodes'].docs.each { |ep|
-      collection_item_url = ep.path[prefixLength..]
+      episodes.each { |ep|
+        collection_item_url = ep.path[prefixLength..]
         ep.data['collection_item_url'] = collection_item_url
 
-        images_path = '/assets/images/episodes/' + ep['sequenceNumber'].to_s.rjust(3, '0') + '/'
+        images_path = '/assets/images/episodes/' + ep['slug'].to_s.rjust(3, '0') + '/'
         ep.data['images'] = (ep.data['images'] || []) +
           site.static_files
             .select { |file| file.relative_path.start_with?(images_path) }
             .map { |file| file.relative_path }
+        
+        previousEp = episodes[ep.data['sequenceNumber'] - 2]
+        nextEp = episodes[ep.data['sequenceNumber']]
+
+        ep.data['previousUrl'] = previousEp&.url
+        ep.data['nextUrl'] = nextEp&.url
       }
 
       Jekyll.logger.info "EpisodeAnnotGen:", "Done."
